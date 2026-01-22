f69fde1fccf6b9e59942c4698e85a61064c30460
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-21 23:51:16 -0600

    updated database.py

--- TinyKey-Value-DB/database.py
+++ TinyKey-Value-DB/database.py
@@ -22,4 +22,38 @@ class DurableHashMap:
 
     def remove(self, key: int):
         self.wal.log_remove(key)         
-        self.map.remove(key)      
+        self.map.remove(key)    
+        
+        
+    def get(self, key: int) -> int:
+        return self.map.get(key)
+
+    def close(self):
+        self.wal.close()
+
+    def checkpoint(self):
+        self.wal.checkpoint(self)
+
+    def dump_as_list(self):
+        """
+        Return the current contents as list of (key,value) pairs.
+        Order is not important for restore speed.
+        """
+        all_items = []
+        for bucket in self.map.hashMap:
+            curr = bucket.bucket.next
+            while curr:
+                k, v = curr.val
+                all_items.append((k, v))
+                curr = curr.next
+        return all_items
+
+if __name__ == "__main__":
+    dh = DurableHashMap()
+    dh.put(1, 10)
+    dh.put(2, 20)
+    dh.remove(1)
+    print(dh.get(2))   
+    dh.checkpoint()    
+    dh.close()
+  
